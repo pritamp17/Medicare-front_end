14c2438d31e2b195c3a4b610badcdfafd9639fa0
pritamp17 <pritampawar625@gmail.com>
2021-10-17 22:27:08 +0530

    added login signup page ,some  styling at signup pages needed

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,45 @@
+// import Button from '@restart/ui/esm/Button';
 import React from 'react'
+import { Form,Button, Card } from 'react-bootstrap';
+import styled from 'styled-components'; 
 
-function Login() {
+function Login(props) {
     return (
+        <>
         <div>
-            
+
+        <Card style={{ width: '20rem' }}  bg="light" border="dark" text="dark">
+         <Card.Body>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>{props.type}'s Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            </Card.Body>
+            <Button variant="link" size="sm" href={props.signUpLink} >New Account Register Here!</Button>
+            </Card>
         </div>
+        
+        </>
     )
 }
-
 export default Login
+
+
+
+ const wrapper = styled.section`
+
+`;
+
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import styled from 'styled-components';
+import Login from '../components/Login';
 
 export default function Home() {
   return (
-    <example>Medicare Front_end</example>
-    
+    <>
+    <Wrapper>
+    <Login type="Patient" signUpLink='patients/signUp' />
+    <Login type="Doctor" signUpLink='doctors/signUp'/>
+  </Wrapper>
+    </>
   )
 }
 
 
-const example = styled.h1`
-color: red;
+
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.div`
+ display: flex;
+ flex: 1;
+ flex-direction: row;
+ justify-content: space-evenly;
+ padding-top:55px;
+ 
 `;
