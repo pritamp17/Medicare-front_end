d5216cb3c922734c79377c14d979025f901b0d0a
pritamp17 <pritampawar625@gmail.com>
2021-10-17 14:34:04 +0530

    backend folder added

---
+++ back-end/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const cors  = require("cors") ;
+
+///app configs
+const app = express();
+const port = process.env.PORT || 9000
+
+
+/// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+app.listen(port,()=>console.log(` listening on localhost:${port}`))
