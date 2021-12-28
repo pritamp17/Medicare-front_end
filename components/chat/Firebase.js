28963b2617b0091129c483f95881240b661bed4e
Pritam Pawar <65030567+pritamp17@users.noreply.github.com>
2021-12-28 11:00:38 +0530

    Update Firebase.js

--- components/chat/Firebase.js
+++ components/chat/Firebase.js
@@ -8,21 +8,9 @@ const firebaseConfig = {
     messagingSenderId: "987760378628",
     appId: "1:987760378628:web:2f879a72c34b394bab0651"
   };
-  let i=7747;
-
- 
-
+  
 //  const firebaseApp = firebase.initializeApp(firebaseConfig)
  const firebaseApp = (!firebase.apps.length) ? firebase.initializeApp(firebaseConfig) : firebase.app();
  const db = firebaseApp.firestore()
-
-// if (!firebase.apps.length) {
-//   db = firebase.initializeApp(firebaseConfig,`${i+500}`).firestore();
-//   i++;
-// }else {
-//   db = firebase.initializeApp(firebaseConfig,`${i+777}`).firestore();; // if already initialized, use that one
-//   i++
-// }
-
 export default db 
 // export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig).firestore() : firebase.app().firestore();
