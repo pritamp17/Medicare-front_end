5fd2df8ccbb38443f98f06c7c5782bf34967b60e
Pritam Pawar <65030567+pritamp17@users.noreply.github.com>
2021-12-28 11:01:21 +0530

    Update Firebase.js

--- components/chat/Firebase.js
+++ components/chat/Firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 const firebaseConfig = {
-     apiKey: "AIzaSyANM9PYBK-4KcvZkJ7iY62oE_COjJKq8DM",
-    authDomain: "medicare-chat-74db9.firebaseapp.com",
+     apiKey: "",
+    authDomain: "",
     projectId: "medicare-chat-74db9",
-    storageBucket: "medicare-chat-74db9.appspot.com",
+    storageBucket: "",
     messagingSenderId: "987760378628",
-    appId: "1:987760378628:web:2f879a72c34b394bab0651"
+    appId: ""
   };
   
 //  const firebaseApp = firebase.initializeApp(firebaseConfig)
